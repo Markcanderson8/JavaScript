b50bb97ff06452f4d86b5b4cda6bd09c3295c19e
Mark <markcanderson8@gmail.com>
2025-03-24 16:50:35 -0400

    setup css text decoration on header link to disappear when on current page

---
+++ practice/js/index.js
@@ -0,0 +1,38 @@
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+currentPage();
+function validateEmail() {
+    const input = document.getElementById('email').value;
+    const ans = emailRegex.test(input);
+
+    if(ans)
+        return true;
+    else
+        return false;       
+}
+
+function currentPage() {
+    switch(document.title) {
+        case "CWA Home Page":
+            // console.log("Home Page");
+            const home = document.getElementById('home');
+            home.setAttribute('style', 'text-decoration: none');
+            break;
+        case "About Us Page":
+            // console.log("About Us Page");
+            const aboutUs = document.getElementById('aboutus');
+            aboutUs.setAttribute('style', 'text-decoration: none');
+            break;
+        case "Contact Us Page":
+            // console.log("Contact Us Page");
+            const contactUs = document.getElementById('contactus');
+            contactUs.setAttribute('style', 'text-decoration: none');
+            break;
+        case "Service Page":
+            console.log("Service Page");
+            const service = document.getElementById('service');
+            service.setAttribute('style', 'text-decoration: none');
+            break;
+        default:
+            console.log("something went wrong");
+    }
+}
